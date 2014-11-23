b50e2095bfc936409568a60dc305c70e4c049a0a
Bo Yang Hu <boyangh@gmail.com>
2014-11-22 23:20:57 -0500

    add new grid content

--- main.js
+++ main.js
@@ -10,28 +10,25 @@ $(window).resize(function () {
 function playClickEffect(element, e) {
     var offset = $(element).offset();
     var left = e.clientX - offset.left;
-    var top = e.clientY - offset.top;
+    var top = e.clientY - offset.top + $(document).scrollTop();
     var dot = document.createElement('div');
     $(dot).addClass("cursor_bump");
     $(dot).css("left", left);
     $(dot).css("top", top);
     $(element).append(dot);
-    console.log($(".cursor_bump").length);
     window.setTimeout(function () {
         $(dot).addClass("bigger");
-    }, 5);
+    }, 100);
 }
 function addClickHandlers() {
     $(".clickable").mousedown(function (e) {
         $(".cursor_bump").remove();
-        playClickEffect(this, e);
-    });
-
-    $(".clickable").mouseup(function (e) {
         var element = this;
-        window.setTimeout(function() {
+        playClickEffect(element, e);
+        setTimeout(function() {
             playClickEffect(element, e);
         }, 500);
-    })
+    });
+
 }
 
